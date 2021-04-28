79584c484faefbffe936b612c196a39bb36e0c6c
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-04-28 21:54:43 +0300

    @/router/Upload add authorization

--- src/router/Upload/index.js
+++ src/router/Upload/index.js
@@ -1,16 +1,23 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import AppLayout from '@/components/AppLayout';
+import LoginPage from '../Login';
 import Header from './Header';
 import Main from './Main';
 
 
 const Upload = function UploadScreen() {
+  const [authorized] = useState(false);
+
   return (
-    <AppLayout headerTitle="Вконтакт">
-      <Header />
-      <Main />
-    </AppLayout>
+    authorized
+      ? (
+        <AppLayout headerTitle="Вконтакт">
+          <Header />
+          <Main />
+        </AppLayout>
+      )
+      : <LoginPage />
   );
 };
 
