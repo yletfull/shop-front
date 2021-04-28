f030cd48a5628bfb9e5b6ae83b6696d77df9c855
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-04-28 22:10:10 +0300

    @/components/Input refactoring

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,36 +7,32 @@ import styles from './styles.module.scss';
 const cssClass = 'input';
 
 const propTypes = {
-  onInput: PropTypes.func,
-  value: PropTypes.string.isRequired,
-  style: PropTypes.objectOf(PropTypes.any),
   className: PropTypes.string,
   placeholder: PropTypes.string,
   fullwidth: PropTypes.bool,
 };
 
 const defaultProps = {
-  onInput: () => {},
-  style: null,
   className: '',
   placeholder: 'Введите текст',
   fullwidth: false,
 };
 
 const Input = function Input(props) {
-  const { value, onInput, style, className, placeholder, fullwidth } = props;
+  const {
+    className, placeholder, fullwidth,
+    ...attrs
+  } = props;
 
   return (
     <input
-      style={style && style}
       className={cx(
         styles[cssClass],
         className,
         fullwidth && styles.fullwidth,
       )}
       placeholder={placeholder}
-      onInput={onInput}
-      value={value}
+      {...attrs}
     />
   );
 };
