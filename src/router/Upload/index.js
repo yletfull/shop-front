ff02f01aa7ab0e277a5bc7a94fb44af732639109
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-04-29 18:51:47 +0300

    check auth fix bug

--- src/router/Upload/index.js
+++ src/router/Upload/index.js
@@ -1,16 +1,24 @@
 
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import AppLayout from '@/components/AppLayout';
 import Spinner from '@/components/Spinner';
 import LoginPage from '../Login';
+import { authCheck } from '../../store/auth/actions';
 import Header from './Header';
 import Main from './Main';
 
 
 const Upload = function UploadScreen() {
+  const dispatch = useDispatch();
+
   const authorized = useSelector((store) => store.auth.user);
   const isFetching = useSelector((store) => store.auth.isFetching);
+
+
+  useEffect(() => {
+    dispatch(authCheck());
+  }, [dispatch]);
 
   if (isFetching) {
     return <Spinner />;
