fa9dc3e367162c1785dddbc55ae6b10253a0c4a3
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-04 12:47:02 +0300

    @/components/ProcessButtonLink feature

---
+++ src/components/ProcessButtonLink/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.scss';
+
+const propTypes = {
+  icon: PropTypes.node,
+  text: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.any),
+  ]),
+  className: PropTypes.string,
+};
+
+const defaultProps = {
+  icon: null,
+  text: 'Имя кнопки',
+  className: '',
+};
+
+const ProcessButton = function ProcessButton(props) {
+  const { icon, text, className, ...attrs } = props;
+
+  return (
+    <Link
+      type="button"
+      className={styles.button}
+      {...attrs}
+    >
+      <span className={styles.icon}>
+        {icon || 'Иконка'}
+      </span>
+      <span className={cx(styles.label, className)}>
+        {Array.isArray(text)
+          ? text.map((el, i) => (
+            <div key={i}>
+              {el}
+            </div>
+          ))
+          : (
+            <span>
+              {text || 'Текст'}
+            </span>
+          )}
+      </span>
+    </Link>
+  );
+};
+
+ProcessButton.propTypes = propTypes;
+ProcessButton.defaultProps = defaultProps;
+
+export default ProcessButton;
