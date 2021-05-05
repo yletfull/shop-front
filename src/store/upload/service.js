7eb0106d9c00a918743d73d87a81f7a8673b87ca
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-05 14:27:01 +0300

    @/store/upload refactoring

--- src/store/upload/service.js
+++ src/store/upload/service.js
@@ -1,22 +1,29 @@
 import api from '@/api';
 
-const fetchAccountsList = () => api.get('api/v1/ad-cabinets').then((data) => data);
+const fetchAccountsList = () => api.get('api/v1/ad-cabinets')
+  .then((data) => data);
 
-const setAccount = ({ cabinetId }) => api.patch(`api/v1/ad-cabinet/${cabinetId}`).then((data) => data);
+const setAccount = ({ cabinetId }) => api.patch(`api/v1/ad-cabinet/${cabinetId}`)
+  .then((data) => data);
 
-const fetchClientsList = ({ cabinetId }) => api.get(`api/v1/ad-cabinet/${cabinetId}/clients`).then((data) => data);
+const fetchClientsList = ({ cabinetId }) => api.get(`api/v1/ad-cabinet/${cabinetId}/clients`)
+  .then((data) => data);
 
-const fetchDocuments = () => api.get('api/v1/documents').then((data) => data);
+const fetchDocuments = () => api.get('api/v1/documents')
+  .then((data) => data);
 
-const fetchDocumentDetails = ({ documentId }) => api.get(`api/v1/document/${documentId}`).then((data) => data);
+const fetchDocumentDetails = ({ documentId }) => api.get(`api/v1/document/${documentId}`)
+  .then((data) => data);
 
-const fetchQueueList = ({ cabinetId, ...params }) => api.get(`api/v1/ad-cabinet/${cabinetId}/queue`, {
-  params,
-}).then((data) => data);
+const fetchQueueList = ({ cabinetId, ...params }) => api
+  .get(`api/v1/ad-cabinet/${cabinetId}/queue`, { params })
+  .then((data) => data);
 
-const uploadFiles = ({ files }) => api.post('api/v1/documents', files).then((data) => data);
+const uploadFiles = ({ files }) => api.post('api/v1/documents', files)
+  .then((data) => data);
 
-const acceptFile = (params) => api.post('api/v1/import', { ...params }).then((data) => data);
+const acceptFile = (params) => api.post('api/v1/import', { ...params })
+  .then((data) => data);
 
 export default {
   fetchAccountsList,
