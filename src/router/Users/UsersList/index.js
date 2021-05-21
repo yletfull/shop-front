4d3674e259fe3ed0ccd967a0e942c8b08110ae1b
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-21 21:06:25 +0300

    @/router/Users/UsersList/ integration API

--- src/router/Users/UsersList/index.js
+++ src/router/Users/UsersList/index.js
@@ -1,51 +1,95 @@
 
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from '@/components/Spinner';
+import { fetchUsers } from '@/store/users/actions';
 
 const Users = function UsersScreen() {
+  const dispatch = useDispatch();
+
+  const [isFetching, setIsFetching] = useState(false);
+
+  const usersData = useSelector((state) => state.users);
+  const users = useRef(usersData);
+  useLayoutEffect(() => {
+    users.current = usersData;
+  }, [usersData]);
+
+  console.log(users.current);
+
+  useEffect(() => async () => {
+    console.log('da');
+    setIsFetching(true);
+    await dispatch(fetchUsers());
+    setIsFetching(false);
+  }, [dispatch]);
+
+  useEffect(() => async () => {
+    console.log('da');
+  }, [dispatch]);
+
+  if (isFetching) {
+    return <Spinner />;
+  }
+
   return (
     <div>
       <table>
-        <tr header="">
-          <td>
-            id
-          </td>
-          <td>
-            login
-          </td>
-          <td>
-            email
-          </td>
-          <td>
-            phone
-          </td>
-          <td>
-            createdAt
-          </td>
-          <td>
-            upldateAt
-          </td>
-        </tr>
-
-        <tr content="">
-          <td>
-            id
-          </td>
-          <td>
-            login
-          </td>
-          <td>
-            email
-          </td>
-          <td>
-            phone
-          </td>
-          <td>
-            createdAt
-          </td>
-          <td>
-            upldateAt
-          </td>
-        </tr>
+        <tbody>
+          <tr header="">
+            <td>
+              Идентификатор
+            </td>
+            <td>
+              Логин
+            </td>
+            <td>
+              e-mail
+            </td>
+            <td>
+              Телефон
+            </td>
+            <td>
+              Дата создания
+            </td>
+            <td>
+              Дата обновления
+            </td>
+          </tr>
+          {users?.current?.length
+            ? users.current.map((user) => (
+              <tr
+                key={user.id}
+                content=""
+              >
+                <td>
+                  {user.id}
+                </td>
+                <td>
+                  {user.login}
+                </td>
+                <td>
+                  {user.email}
+                </td>
+                <td>
+                  {user.phone}
+                </td>
+                <td>
+                  {user.createdAt}
+                </td>
+                <td>
+                  {user.upldateAt}
+                </td>
+              </tr>
+            ))
+            : (
+              <tr content="">
+                <td colSpan="8">
+                  Пользователи не найдены
+                </td>
+              </tr>
+            )}
+        </tbody>
       </table>
     </div>
   );
