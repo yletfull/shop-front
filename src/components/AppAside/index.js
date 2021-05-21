b0e3a42ba349a4fbe816333698841f9bdc89877a
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-21 15:14:35 +0300

    @/components/AppAside refactoring

--- src/components/AppAside/index.js
+++ src/components/AppAside/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import IconLogo from '@/icons/Logo';
 import IconLogout from '@/icons/Logout';
+import MenuItem from '@/components/MenuItem';
+import { patchs, titles } from '@/router/routes';
 import { authSignOut } from '../../store/auth/actions';
 import styles from './styles.module.scss';
-
 
 const AppAside = function AppAside() {
   const dispatch = useDispatch();
@@ -13,10 +15,27 @@ const AppAside = function AppAside() {
     dispatch(authSignOut());
   };
 
+  const menuItemsList = [
+    {
+      title: titles.roles,
+      to: patchs.roles,
+      icon: <IconLogout />,
+    },
+  ];
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapperTop}>
         <IconLogo />
+        {menuItemsList.map((menuItem, ind) => (
+          <MenuItem
+            key={ind}
+            to={menuItem.to}
+            icon={menuItem.icon}
+            title={menuItem.title}
+          />
+        ))}
+
       </div>
       <div className={styles.wrapperBottom}>
         <button
