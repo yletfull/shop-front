2dd176bdf8c08c01dae81f1ba9b497fecbf5f4bd
Marat <mamirov@ubic.tech>
2021-05-21 17:41:29 +0300

    ADV-17: SegmentsList: create component

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,30 @@
+import SegmentsList from './SegmentsList';
 import Upload from './Upload';
 
 export const patchs = {
   upload: '/',
+  segments: '/segments',
 };
 
 export const titles = {
   upload: 'Загрузка',
+  segments: 'Сегменты',
 };
 
 
 export default [
   {
+    exact: true,
     path: patchs.upload,
     redirect: patchs.upload,
     Component: Upload,
     meta: {
       title: titles.main,
     },
+  },
+  {
+    Component: SegmentsList,
+    path: patchs.segments,
+    title: titles.segments,
   },
 ];
