850976a84cf183dcd1cc9bcb7bc9a8d61993b3f6
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-21 16:37:06 +0300

    @/router/Upload refactoring

--- src/router/Upload/index.js
+++ src/router/Upload/index.js
@@ -1,37 +1,15 @@
 
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Spinner from '@/components/Spinner';
-import LoginPage from '../Login';
-import { authCheck } from '../../store/auth/actions';
+import React from 'react';
 import Header from './Header';
 import Main from './Main';
 
 
 const Upload = function UploadScreen() {
-  const dispatch = useDispatch();
-
-  const authorized = useSelector((store) => store.auth.user);
-  const isFetching = useSelector((store) => store.auth.isFetching);
-
-
-  useEffect(() => {
-    dispatch(authCheck());
-  }, [dispatch]);
-
-  if (isFetching) {
-    return <Spinner />;
-  }
-
   return (
-    authorized
-      ? (
-        <React.Fragment headerTitle="Вконтакт">
-          <Header />
-          <Main />
-        </React.Fragment>
-      )
-      : <LoginPage />
+    <React.Fragment headerTitle="Вконтакт">
+      <Header />
+      <Main />
+    </React.Fragment>
   );
 };
 
