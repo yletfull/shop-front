2e479da1e36b72ef123079edc2700f57beb4334d
Marat <mamirov@ubic.tech>
2021-05-24 14:35:24 +0300

    ADV-17: SegmentsList/TableView: add filter

--- src/router/SegmentsList/TableView/index.js
+++ src/router/SegmentsList/TableView/index.js
@@ -1,19 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { useHistory } from 'react-router-dom';
+import { useQuery } from '@/hooks';
 import { formatNumber } from '@/utils/format';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import styles from './styles.module.scss';
 
 const propTypes = {
   data: PropTypes.arrayOf(PropTypes.string),
+  onFilter: PropTypes.func,
 };
 
 const defaultProps = {
   data: [],
+  onFilter: () => {},
 };
 
-const TableView = functio TableView({ data }) {
+const TableView = function TableView({ data, onFilter }) {
+  const queryParams = {
+    searchId: 'id',
+    searchName: 'name',
+  };
+
+  const history = useHistory();
+  const query = useQuery();
+
+  const [
+    searchId,
+    setSearchId,
+  ] = useState(query.get(queryParams.searchId) || '');
+  const [
+    searchName,
+    setSearchName,
+  ] = useState(query.get(queryParams.searchName) || '');
+
+  const handleChangeSearchId = (e) => {
+    const { value } = e?.target || {};
+    if (typeof value === 'undefined') {
+      return;
+    }
+    setSearchId(value);
+  };
+  const handleChangeSearchName = (e) => {
+    const { value } = e?.target || {};
+    if (typeof value === 'undefined') {
+      return;
+    }
+    setSearchName(value);
+  };
+  const handleClickSearchButton = () => {
+    query.set(queryParams.searchId, searchId);
+    query.set(queryParams.searchName, searchName);
+    history.push({ search: query.toString() });
+    onFilter();
+  };
+
   return (
-    <table className={styles.table}>
+    <table className={styles.tableView}>
       <tbody>
+        <tr>
+          <td data-purpose="filter">
+            <span className={styles.tableViewCell}>
+              <Input
+                className={styles.tableViewInput}
+                placeholder="ID"
+                type="text"
+                value={searchId}
+                fullwidth
+                onChange={handleChangeSearchId}
+              />
+            </span>
+          </td>
+          <td
+            colSpan="8"
+            data-purpose="filter"
+          >
+            <span className={styles.tableViewCell}>
+              <Input
+                className={cx(
+                  styles.tableViewInput,
+                  styles.tableViewInput_grow
+                )}
+                placeholder="Название"
+                type="text"
+                value={searchName}
+                onChange={handleChangeSearchName}
+              />
+              <Button
+                type="button"
+                onClick={handleClickSearchButton}
+              >
+                Найти
+              </Button>
+            </span>
+          </td>
+        </tr>
+
         <tr>
           <th>
             ID
