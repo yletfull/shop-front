0b732da54b2e87f3bef0cdc7e2252174fbcf9311
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-24 12:13:42 +0300

    @/router/User add list and roles details

--- src/router/Users/UsersList/index.js
+++ src/router/Users/UsersList/index.js
@@ -1,96 +1,23 @@
 
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import dayjs from '@/utils/day';
-import Spinner from '@/components/Spinner';
-import { fetchUsers } from '@/store/users/actions';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import routes from './routes';
+
 
 const Users = function UsersScreen() {
-  const dispatch = useDispatch();
-
-  const [isFetching, setIsFetching] = useState(false);
-
-  const usersList = useSelector((state) => state.users.list);
-  const users = useRef(usersList);
-  useLayoutEffect(() => {
-    users.current = usersList;
-  }, [usersList]);
-
-  useEffect(() => {
-    const fetchUsersFn = async () => {
-      setIsFetching(true);
-      await dispatch(fetchUsers());
-      setIsFetching(false);
-    };
-    fetchUsersFn();
-  }, [dispatch]);
-
-  if (isFetching) {
-    return <Spinner />;
-  }
-
   return (
     <div>
-      <table>
-        <tbody>
-          <tr header="">
-            <td>
-              Идентификатор
-            </td>
-            <td>
-              Логин
-            </td>
-            <td>
-              e-mail
-            </td>
-            <td>
-              Телефон
-            </td>
-            <td>
-              Дата создания
-            </td>
-            <td>
-              Дата обновления
-            </td>
-          </tr>
-          {users?.current?.length
-            ? users.current.map((user) => (
-              <tr
-                key={user.id}
-                content=""
-              >
-                <td>
-                  <Link to={(location) => ({ location, pathname: `${user.id}/details` })}>
-                    {user.id || '-'}
-                  </Link>
-                </td>
-                <td>
-                  {user.login || '-'}
-                </td>
-                <td>
-                  {user.email || '-'}
-                </td>
-                <td>
-                  {user.phone || '-'}
-                </td>
-                <td>
-                  {dayjs(user.createdAt, 'YYYY:MM:DD HH:mm:ss')}
-                </td>
-                <td>
-                  {dayjs(user.uploadAt, 'YYYY:MM:DD HH:mm:ss')}
-                </td>
-              </tr>
-            ))
-            : (
-              <tr content="">
-                <td colSpan="8">
-                  Пользователи не найдены
-                </td>
-              </tr>
-            )}
-        </tbody>
-      </table>
+      <Switch>
+        {routes.map(({ exact, path, title, Component }) => (
+          <Route
+            key={`h_${path}`}
+            path={path}
+            exact={Boolean(exact)}
+          >
+            <Component title={title} />
+          </Route>
+        ))}
+      </Switch>
     </div>
   );
 };
