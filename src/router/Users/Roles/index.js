3bb2c4643de0dccedf7357bc9aee660563e29e1d
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-24 10:33:49 +0300

    @/router/Users/Roles/index add table

--- src/router/Users/Roles/index.js
+++ src/router/Users/Roles/index.js
@@ -1,19 +1,76 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from '@/components/Spinner';
+import { fetchRoles } from '@/store/users/actions';
 
-const Roles = function RolesScreen() {
-  useEffect(() => async () => {
-    // console.log('roles');
-  }, []);
+const Users = function UsersScreen() {
+  const dispatch = useDispatch();
 
+  const [isFetching, setIsFetching] = useState(false);
+
+  const rolesList = useSelector((state) => state.users.list);
+  const roles = useRef(rolesList);
+  useLayoutEffect(() => {
+    roles.current = rolesList;
+  }, [rolesList]);
+
+  useEffect(() => {
+    const fetchUsersFn = async () => {
+      setIsFetching(true);
+      await dispatch(fetchRoles());
+      setIsFetching(false);
+    };
+    fetchUsersFn();
+  }, [dispatch]);
+
+  if (isFetching) {
+    return <Spinner />;
+  }
 
   return (
     <div>
-      <p>
-        Roles
-      </p>
+      <table>
+        <tbody>
+          <tr header="">
+            <td>
+              Идентификатор
+            </td>
+            <td>
+              Name
+            </td>
+            <td>
+              Title
+            </td>
+          </tr>
+          {roles?.current?.length
+            ? roles.current.map((role) => (
+              <tr
+                key={role.id}
+                content=""
+              >
+                <td>
+                  {role.id || '-'}
+                </td>
+                <td>
+                  {role.name || '-'}
+                </td>
+                <td>
+                  {role.title || '-'}
+                </td>
+              </tr>
+            ))
+            : (
+              <tr content="">
+                <td colSpan="8">
+                  Роли не найдены
+                </td>
+              </tr>
+            )}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default Roles;
+export default Users;
