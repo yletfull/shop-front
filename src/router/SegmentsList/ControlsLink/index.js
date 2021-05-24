1ceccece4a468dba279cd0899572196af0205f24
Marat <mamirov@ubic.tech>
2021-05-24 17:41:54 +0300

    adv-17: SegmentsList/ControlsLink: updata component

--- src/router/SegmentsList/ControlsLink/index.js
+++ src/router/SegmentsList/ControlsLink/index.js
@@ -1,43 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import IconPlus from '@/icons/Plus';
-import IconSearch from '@/icons/Search';
 import styles from './styles.module.scss';
-
-const types = {
-  create: 'create',
-  search: 'search',
-};
 
 const propTypes = {
   children: PropTypes.node,
+  icon: PropTypes.node,
   to: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(Object.values(types)),
 };
 
 const defaultProps = {
   children: null,
-  type: '',
+  icon: null,
 };
 
-const ControlsLink = function ControlsLink({ children, to, type, ...props }) {
-  const Icon = {
-    [types.create]: IconPlus,
-    [types.search]: IconSearch,
-  }[type] || null;
-
+const ControlsLink = function ControlsLink({ children, to, icon, ...props }) {
   return (
     <Link
       className={styles.controlsLink}
       to={to}
       {...props}
     >
-      <span className={styles.controlsLinkIcon}>
-        {Icon && (
-          <Icon />
-        )}
-      </span>
+      {icon && (
+        <span className={styles.controlsLinkIcon}>
+          {icon}
+        </span>
+      )}
       {children}
     </Link>
   );
