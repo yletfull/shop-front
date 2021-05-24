177a758f67e9e81e74419356ae35921512d699f2
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-24 14:48:10 +0300

    @/router/Users/UserList/details/index feature add role form

--- src/router/Users/UsersList/details/index.js
+++ src/router/Users/UsersList/details/index.js
@@ -4,10 +4,11 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import Spinner from '@/components/Spinner';
-import { fetchUserDetails, fetchUserRoles } from '@/store/users/actions';
+import { fetchUserDetails, fetchUserRoles, fetchAllRoles } from '@/store/users/actions';
 import dayjs from '@/utils/day';
 import Tag from '@/components/Tag';
 import Button from '@/components/Button';
+import Select from '@/components/Select';
 import TimesCircleIcon from '@/icons/TimesCircle';
 import styles from './styles.module.scss';
 
@@ -16,6 +17,7 @@ const Details = function RolesDetailsScreen() {
   const dispatch = useDispatch();
 
   const [isFetching, setIsFetching] = useState(false);
+  const [selectedRole, setSelectedRole] = useState();
 
   const userData = useSelector((state) => state.users.userDetails);
   const userDetails = useRef(userData);
@@ -29,16 +31,39 @@ const Details = function RolesDetailsScreen() {
     userRoles.current = userRolesData;
   }, [userRolesData]);
 
+  const allRolesData = useSelector((state) => state.users.allRoles);
+  const allRoles = useRef(allRolesData);
+  useLayoutEffect(() => {
+    allRoles.current = allRolesData;
+  }, [allRolesData]);
+
   const { userId } = useParams();
   useEffect(() => {
     const fetchUsersFn = async () => {
       setIsFetching(true);
       await dispatch(fetchUserDetails({ userId }));
       await dispatch(fetchUserRoles({ userId }));
+      await dispatch(fetchAllRoles());
       setIsFetching(false);
     };
     fetchUsersFn();
   }, [dispatch, userId]);
+
+  const handleRoleChange = (e) => {
+    const { value } = e.target;
+    setSelectedRole(value);
+  };
+
+  const getAllRolesOptions = () => {
+    if (allRoles.current?.length) {
+      return allRoles.current.map((role) => ({
+        text: role.title,
+        value: role.name,
+      }));
+    }
+    return [];
+  };
+
 
   if (isFetching) {
     return <Spinner />;
@@ -110,7 +135,7 @@ const Details = function RolesDetailsScreen() {
             <td>
               <div className={styles.tagsWrapper}>
                 {userRoles.current?.length
-                  && userRoles.current.map((role, ind) => (
+                  ? userRoles.current.map((role, ind) => (
                     <div
                       className={styles.roleItem}
                       key={ind}
@@ -123,8 +148,36 @@ const Details = function RolesDetailsScreen() {
                         <TimesCircleIcon />
                       </Button>
                     </div>
-                  ))}
+                  ))
+                  : (
+                    <span>
+                      -
+                    </span>
+                  )}
               </div>
+            </td>
+          </tr>
+
+          <tr content="">
+            <td>
+              Добавить роль
+            </td>
+            <td>
+              <form>
+                <Select
+                  value={selectedRole}
+                  options={getAllRolesOptions(allRoles.current)}
+                  onChange={handleRoleChange}
+                  resetText="Не выбрано"
+                  placeholder="Выбрать роль"
+                  className={styles.select}
+                  disabled={!allRoles.current?.length}
+                />
+                <Button className={styles.addRoleButton}>
+                  Добавить роль
+                </Button>
+              </form>
+
             </td>
           </tr>
 
