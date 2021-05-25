bc61728887354304a588263c04f5d0711b984210
Marat <mamirov@ubic.tech>
2021-05-25 14:25:56 +0300

    adv-22: set header from components

--- src/router/Upload/index.js
+++ src/router/Upload/index.js
@@ -1,16 +1,34 @@
-
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { setHeader } from '@/store/ui/actions';
 import Header from './Header';
 import Main from './Main';
 
+const propTypes = {
+  defaultTitle: PropTypes.string,
+};
 
-const Upload = function UploadScreen() {
+const defaultProps = {
+  defaultTitle: '',
+};
+
+const Upload = function UploadScreen({ defaultTitle }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setHeader(defaultTitle));
+  }, [dispatch, defaultTitle]);
+
   return (
-    <React.Fragment headerTitle="Вконтакт">
+    <Fragment>
       <Header />
       <Main />
-    </React.Fragment>
+    </Fragment>
   );
 };
+
+Upload.propTypes = propTypes;
+Upload.defaultProps = defaultProps;
 
 export default Upload;
