7144fbc0a173562e9095852f194fb1a9e6d9a61c
Marat <mamirov@ubic.tech>
2021-05-25 14:22:31 +0300

    adv-22: App: set header from store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import WithAuth from '@/components/AppWithAuth';
 import WithAppLayout from '@/components/AppLayout';
+import { getHeader } from '@/store/ui/selectors';
 import RouterView from './router/RouterView';
 
 const App = function App() {
+  const header = useSelector(getHeader);
+
   return (
     <WithAuth>
-      <WithAppLayout>
+      <WithAppLayout headerTitle={header}>
         <RouterView />
       </WithAppLayout>
     </WithAuth>
