d688dabf4fec39cbb36d863628eedff1423977c5
Marat <mamirov@ubic.tech>
2021-05-26 14:59:55 +0300

    adv-23: create modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IconTimes from '@/icons/Times';
+import styles from './styles.module.scss';
+
+const propTypes = {
+  children: PropTypes.node,
+  isVisible: PropTypes.bool,
+  header: PropTypes.node,
+  onClose: PropTypes.func,
+};
+const defaultProps = {
+  children: null,
+  isVisible: false,
+  header: null,
+  onClose: () => {},
+};
+
+const Modal = function Modal({
+  children,
+  isVisible,
+  header,
+  onClose,
+}) {
+  const handleClickCloseButton = () => onClose();
+
+  return (
+    <div className={styles.wrapper}>
+      {isVisible && (
+        <div className={styles.modal}>
+          <div className={styles.modalHeader}>
+            {header}
+            <button
+              type="button"
+              className={styles.modalClose}
+              onClick={handleClickCloseButton}
+            >
+              <IconTimes />
+            </button>
+          </div>
+          <div className={styles.modalContent}>
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Modal.propTypes = propTypes;
+Modal.defaultProps = defaultProps;
+
+export default Modal;
