444864a0270d96ede53c9a3dfdff692791f3c968
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-05-26 21:06:37 +0300

    @/router/Upload/Header/Templates/index refactoring

--- src/router/Upload/Header/Tempaltes/index.js
+++ src/router/Upload/Header/Tempaltes/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable prefer-destructuring */
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import cx from 'classnames';
 import { formatDate } from '@/utils/format';
@@ -12,13 +13,16 @@ import IconDownload from '@/icons/Download';
 import IconUpload from '@/icons/Upload';
 import Spinner from '@/components/Spinner';
 
-import { firstUploadStages, globalStages } from '../../stages';
 import {
+  setStage,
+  uploadFiles,
+  fetchRecentFile,
   fetchDocumentDetails,
-  fetchDocuments, setStage,
-  setUploadedFiles, uploadFiles,
-} from '../../../../store/upload/actions';
+  documentDetails, fetchDocuments,
+} from '@/store/upload/actions';
+import { firstUploadStages, globalStages } from '../../stages';
 import styles from './styles.module.scss';
+
 
 const getHeaderTempalteContent = (data) => [[
   { title: 'Последнее скачивание', value: '-', id: 0 },
@@ -37,125 +41,171 @@ const Header = function HeaderScreen() {
   const fileInput = useRef(null);
 
   const [fileIsLoaded, setFileIsLoaded] = useState(false);
+  const [fileIsLoading, setFileIsLoading] = useState(false);
   const [detailsIsFetching, setDetailsIsFetching] = useState(false);
 
-  const stage = useSelector((state) => state.upload.stage);
-  const documents = useSelector(
+  const [recentFileDetails, setRecentFileDetails] = useState({});
+  const [uploadedFileDetails, setUploadedFileDetails] = useState({});
+
+  const recentFileData = useSelector(
+    (state) => state.upload?.recentFile
+  );
+  const recentFile = useRef(recentFileData);
+  useLayoutEffect(() => {
+    recentFile.current = recentFileData;
+  }, [recentFileData]);
+
+
+  const fileDetailsData = useSelector(
+    (state) => state.upload?.documentDetails
+  );
+  const fileDetails = useRef(fileDetailsData);
+  useLayoutEffect(() => {
+    fileDetails.current = fileDetailsData;
+  }, [fileDetailsData]);
+
+
+  const uploadedFilesData = useSelector(
+    (state) => state.upload?.uploadedFiles || []
+  );
+  const uploadedFiles = useRef(uploadedFilesData);
+  useLayoutEffect(() => {
+    uploadedFiles.current = uploadedFilesData;
+  }, [uploadedFilesData]);
+
+
+  const allUploadedFilesData = useSelector(
     (state) => state.upload?.documents
-  ) || '';
-  const uploadedFiles = useSelector(
-    (state) => state.upload?.uploadedFiles
-  ) || '';
+  );
+  const allUploadedFiles = useRef(allUploadedFilesData);
+  useLayoutEffect(() => {
+    allUploadedFiles.current = allUploadedFilesData;
+  }, [allUploadedFilesData]);
+
 
   const submitFile = async (data) => {
-    setFileIsLoaded(true);
-    dispatch(setStage(firstUploadStages.fileIsLoaded));
+    setFileIsLoading(true);
+    dispatch(setStage(firstUploadStages.fileIsLoading));
     await dispatch(uploadFiles(data));
-    if (Object.keys(uploadedFiles).length) {
+    if (uploadedFiles.current?.length) {
       dispatch(setStage(firstUploadStages.selectList));
     } else {
       dispatch(setStage(globalStages.errorCheck));
     }
-    setFileIsLoaded(false);
+    setFileIsLoading(false);
   };
-
   const handleFileChange = (e) => {
     const data = new FormData();
     data.append('file', e.target.files[0]);
     data.append('filename', e.target.files[0].name);
     submitFile(data);
   };
-
   const handleSelectFileButtonClick = () => {
     fileInput.current.click();
   };
 
-  const firstStage = stage === firstUploadStages.selectAccount;
-  const documentsIsNotLoading = documents.length === 0;
+  useEffect(() => (async () => {
+    setDetailsIsFetching(true);
 
-  // useEffect(() => (async () => {
-  //   if (documents.length > 0) {
-  //     dispatch(fetchDocumentDetails(documents[documents.length - 1].id));
-  //   } else {
-  //     dispatch(setStage(firstUploadStages.filseIsNotLoaded));
-  //   }
-  // })(), [dispatch, documents]);
+    await dispatch(fetchDocuments());
+    if (allUploadedFiles.current?.length) {
+      await dispatch(fetchDocumentDetails(allUploadedFiles.current[0]?.id));
+      if (documentDetails.current) {
+        setUploadedFileDetails(documentDetails.current);
+      }
+    }
+
+    await dispatch(fetchRecentFile());
+    if (recentFile.current?.length) {
+      await dispatch(fetchDocumentDetails(recentFile.current?.id));
+      if (documentDetails.current) {
+        setRecentFileDetails(documentDetails.current);
+      }
+    }
+
+    setDetailsIsFetching(false);
+  })(), [dispatch]);
+
+  useEffect(() => {
+    if (recentFileDetails.current?.length) {
+      return setFileIsLoaded(true);
+    }
+    setFileIsLoaded(false);
+  }, [recentFileDetails]);
 
   return (
-    !firstStage
-        && (
-          <div className={styles.headerTemplatesWrapper}>
-            {detailsIsFetching
-              ? <Spinner />
-              : (
-                <React.Fragment>
-                  <HeaderTemplate className={styles.headerTemplate}>
-                    <ProcessButtonLink
-                      icon={<IconDownload />}
-                      text={['Скачать', 'файл']}
-                      to={`/api/v1/document/${uploadedFiles[0].id}/raw`}
-                      target="_blank"
-                      download
-                      disabled={fileIsLoaded || documentsIsNotLoading}
-                    />
-                    <div>
-                      {getHeaderTempalteContent(uploadedFiles[0])[0]
-                        .map(({ title, value, id }) => (
-                          <div
-                            className={styles.textWrapper}
-                            key={id}
-                          >
-                            <span>
-                              {title}
-                            </span>
-                            <span className={styles.value}>
-                              {value}
-                            </span>
-                          </div>
-                        ))}
+    <div className={styles.headerTemplatesWrapper}>
+      {detailsIsFetching
+        ? <Spinner />
+        : (
+          <React.Fragment>
+            <HeaderTemplate className={styles.headerTemplate}>
+              <ProcessButtonLink
+                icon={<IconDownload />}
+                text={['Скачать', 'файл']}
+                to={`/api/v1/document/${recentFile.current.id}/raw`}
+                target="_blank"
+                download
+                disabled={!fileIsLoaded}
+              />
+              <div>
+                {getHeaderTempalteContent(recentFileDetails)[0]
+                  .map(({ title, value, id }) => (
+                    <div
+                      className={styles.textWrapper}
+                      key={id}
+                    >
+                      <span>
+                        {title}
+                      </span>
+                      <span className={styles.value}>
+                        {value}
+                      </span>
                     </div>
-                  </HeaderTemplate>
-                  <HeaderTemplate className={styles.headerTemplate}>
-                    <label>
-                      <ProcessButton
-                        icon={<IconUpload />}
-                        text={['Загрузить', 'файл']}
-                        onClick={handleSelectFileButtonClick}
-                        disabled={fileIsLoaded}
-                      />
-                      <input
-                        type="file"
-                        id="fileInput"
-                        style={{ display: 'none' }}
-                        ref={fileInput}
-                        onChange={handleFileChange}
-                      />
-                    </label>
-                    <div>
-                      {getHeaderTempalteContent(uploadedFiles[0])[1]
-                        .map(({ title, value, id, valueColor }) => (
-                          <div
-                            className={styles.textWrapper}
-                            key={id}
-                          >
-                            <span>
-                              {title}
-                            </span>
-                            <span
-                              className={cx(
-                                styles.value, { [valueColor]: valueColor }
-                              )}
-                            >
-                              {value}
-                            </span>
-                          </div>
-                        ))}
+                  ))}
+              </div>
+            </HeaderTemplate>
+            <HeaderTemplate className={styles.headerTemplate}>
+              <label>
+                <ProcessButton
+                  icon={<IconUpload />}
+                  text={['Загрузить', 'файл']}
+                  onClick={handleSelectFileButtonClick}
+                  disabled={fileIsLoading}
+                />
+                <input
+                  type="file"
+                  id="fileInput"
+                  style={{ display: 'none' }}
+                  ref={fileInput}
+                  onChange={handleFileChange}
+                />
+              </label>
+              <div>
+                {getHeaderTempalteContent(uploadedFileDetails)[1]
+                  .map(({ title, value, id, valueColor }) => (
+                    <div
+                      className={styles.textWrapper}
+                      key={id}
+                    >
+                      <span>
+                        {title}
+                      </span>
+                      <span
+                        className={cx(
+                          styles.value, { [valueColor]: valueColor }
+                        )}
+                      >
+                        {value}
+                      </span>
                     </div>
-                  </HeaderTemplate>
-                </React.Fragment>
-              )}
-          </div>
-        )
+                  ))}
+              </div>
+            </HeaderTemplate>
+          </React.Fragment>
+        )}
+    </div>
+
   );
 };
 
