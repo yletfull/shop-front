dbe77a3a805b410277585ec499ff0fc4b9acb8e0
Marat <mamirov@ubic.tech>
2021-05-28 12:16:49 +0300

    adv-27: AudienceDetails: add store

--- src/router/AudiencesDetails/index.js
+++ src/router/AudiencesDetails/index.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { injectReducer } from '@/store';
 import { setHeader } from '@/store/ui/actions';
+import Spinner from '@/components/Spinner';
+import { namespace as NS } from './constants';
+import reducer from './reducer';
+import { getIsFetchingData, getData } from './selectors';
+import CommonInfo from './CommonInfo';
+import Comparison from './Comparison';
 import styles from './styles.module.scss';
 
 const propTypes = {
@@ -15,13 +22,34 @@ const defaultProps = {
 const AudiencesDetails = function AudiencesDetails({ defaultTitle }) {
   const dispatch = useDispatch();
 
+  const isFetching = useSelector(getIsFetchingData);
+  const data = useSelector(getData);
+
+  useEffect(() => {
+    injectReducer(NS, reducer);
+  }, []);
+
   useEffect(() => {
     dispatch(setHeader(defaultTitle));
   }, [dispatch, defaultTitle]);
 
   return (
-    <div className={styles.wrapper}>
-      AudiencesDetails
+    <div className={styles.audienceDetails}>
+      {isFetching && (
+        <Spinner />
+      )}
+
+      {!isFetching && (
+        <Fragment>
+          <CommonInfo data={data} />
+
+          <h2>
+            Сравнение с глобальной аудиторией
+          </h2>
+
+          <Comparison data={data} />
+        </Fragment>
+      )}
     </div>
   );
 };
