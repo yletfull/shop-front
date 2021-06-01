b55bcc3c0c755f73a597d287623b74f91563c766
Marat <mamirov@ubic.tech>
2021-06-01 16:30:06 +0300

    adv-29: add useDebounce hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useDebounce } from './use-debounce';
 export { default as useKeyPress } from './use-key-press';
 export { default as useOnClickOutside } from './use-on-click-outside';
 export { default as useQuery } from './use-query';
