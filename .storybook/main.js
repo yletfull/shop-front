28d106225af87c96efdc322fc2a504ccb0a8b771
Mikhail Panichev <denis.invader@gmail.com>
2021-06-02 14:26:21 +0300

    storybook: update config, add @ alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
+const projectWebpackConfig = require('../webpack.config');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/stories.mdx',
+    '../src/**/*.stories.js',
+    '../src/**/stories.js',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = projectWebpackConfig.resolve.alias['@'];
+
+    return config;
+  },
 }
