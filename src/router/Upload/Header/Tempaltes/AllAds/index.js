3d12264a26c6bd58f2acc3dabbbd15675462cf17
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-06-03 19:43:11 +0300

    @/router/Upload/Header/Templates refactoring and add new item

---
+++ src/router/Upload/Header/Tempaltes/AllAds/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import ProcessButtonLink from '@/components/ProcessButtonLink';
+import HeaderTemplate from '@/components/HeaderTemplate';
+import IconDownload from '@/icons/Download';
+
+const Header = function HeaderScreen() {
+  const selectAccount = useSelector(
+    (state) => state.upload?.selectAccount
+  ) || '';
+  const selectClient = useSelector(
+    (state) => state.upload?.selectClient
+  ) || '';
+
+  return (
+    <HeaderTemplate>
+      <ProcessButtonLink
+        icon={<IconDownload />}
+        text={['Скачать', 'все объявления']}
+        to={`/api/v1/import?cabinetId=${selectAccount}&clientId=${selectClient}`}
+        target="_blank"
+        download
+        disabled={!(selectAccount && selectClient)}
+      />
+    </HeaderTemplate>
+  );
+};
+
+export default Header;
---
+++ src/router/Upload/Header/Tempaltes/LastProcessed/index.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { formatDate } from '@/utils/format';
+
+import ProcessButtonLink from '@/components/ProcessButtonLink';
+import HeaderTemplate from '@/components/HeaderTemplate';
+import IconDownload from '@/icons/Download';
+import Spinner from '@/components/Spinner';
+
+import {
+  fetchRecentFile,
+  fetchDocumentDetails,
+  fetchDocuments,
+  setParentDocument,
+} from '@/store/upload/actions';
+import styles from './styles.module.scss';
+
+
+const getHeaderTempalteContent = (data) => [
+  { title: 'Последняя загрузка во ВК', value: formatDate(data.createdAt, 'DD.MM.YYYY HH:MM:ss'), id: 0 },
+  { title: 'Всего РК', value: data.data?.correct, id: 1 },
+  { title: 'Новых РК', value: data.data?.new, id: 2 },
+  { title: 'Ошибок', value: data.data?.incorrect, valueColor: 'red', id: 3 },
+];
+
+
+const Header = function HeaderScreen() {
+  const dispatch = useDispatch();
+
+  const [fileIsLoaded, setFileIsLoaded] = useState(false);
+  const [detailsIsFetching, setDetailsIsFetching] = useState(false);
+
+  const recentFileData = useSelector(
+    (state) => state.upload?.recentFile
+  );
+  const recentFile = useRef(recentFileData);
+  useLayoutEffect(() => {
+    recentFile.current = recentFileData;
+  }, [recentFileData]);
+
+  const recentFileIsLoading = useSelector(
+    (store) => store.upload?.recentFileIsLoading
+  );
+
+  const fileDetailsData = useSelector(
+    (state) => state.upload?.documentDetails
+  );
+  const fileDetails = useRef(fileDetailsData);
+  useLayoutEffect(() => {
+    fileDetails.current = fileDetailsData;
+  }, [fileDetailsData]);
+
+
+  const uploadedFilesData = useSelector(
+    (state) => state.upload?.uploadedFiles || []
+  );
+  const uploadedFiles = useRef(uploadedFilesData);
+  useLayoutEffect(() => {
+    uploadedFiles.current = uploadedFilesData;
+  }, [uploadedFilesData]);
+
+
+  const allUploadedFilesData = useSelector(
+    (state) => state.upload?.documents
+  );
+  const allUploadedFiles = useRef(allUploadedFilesData);
+  useLayoutEffect(() => {
+    allUploadedFiles.current = allUploadedFilesData;
+  }, [allUploadedFilesData]);
+
+
+  const getParentDocumentState = (documents) => documents
+    .find((doc) => doc.sequenceId === 0);
+
+
+  useEffect(() => (async () => {
+    setDetailsIsFetching(true);
+
+    await dispatch(fetchRecentFile());
+
+    await dispatch(fetchDocuments());
+    if (allUploadedFiles.current?.length) {
+      const parentDoc = getParentDocumentState(allUploadedFiles.current);
+      await dispatch(fetchDocumentDetails(parentDoc.id));
+      if (fileDetails.current) {
+        dispatch(setParentDocument(fileDetails.current));
+      }
+    }
+
+    setDetailsIsFetching(false);
+  })(), [dispatch]);
+
+
+  useEffect(() => {
+    if (Object.keys(recentFileData).length) {
+      return setFileIsLoaded(true);
+    }
+    setFileIsLoaded(false);
+  }, [recentFileData]);
+
+  return (
+    detailsIsFetching
+      ? <Spinner />
+      : (
+        <HeaderTemplate>
+          <ProcessButtonLink
+            icon={<IconDownload />}
+            text={['Скачать', 'последний файл']}
+            to={`/api/v1/document/${recentFileData.id}/raw`}
+            target="_blank"
+            download
+            disabled={recentFileIsLoading || !fileIsLoaded}
+          />
+          <div>
+            {getHeaderTempalteContent(recentFileData)
+              .map(({ title, value, id }) => (
+                <div
+                  className={styles.textWrapper}
+                  key={id}
+                >
+                  <span>
+                    {title}
+                  </span>
+                  <span className={styles.value}>
+                    {value}
+                  </span>
+                </div>
+              ))}
+          </div>
+        </HeaderTemplate>
+
+      )
+
+  );
+};
+
+export default Header;
