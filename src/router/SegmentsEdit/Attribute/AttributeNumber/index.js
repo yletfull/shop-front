6c1af924659e72e2337fbce7338105497edcd5db
Marat <mamirov@ubic.tech>
2021-06-08 17:30:15 +0300

    adv-29: SegmentsEdit/Attribute/AttributeNumber: use formik

--- src/router/SegmentsEdit/Attribute/AttributeNumber/index.js
+++ src/router/SegmentsEdit/Attribute/AttributeNumber/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Input from '@/components/Input';
+import { Formik, Form, Field } from 'formik';
+import { formatNumber } from '@/utils/format';
+import AttributeNumverInput from './AttributeNumberInput';
 import styles from './styles.module.scss';
 
 const propTypes = {
@@ -21,18 +23,81 @@ const AttributeNumber = function AttributeNumber({
   data,
 }) {
   const { maxValue, minValue } = data || {};
+
+  const fieldNames = {
+    min: 'min',
+    max: 'max',
+  };
+
+  const initialFormValues = {
+    min: minValue,
+    max: maxValue,
+  };
+
+  const handleSubmitForm = (values, { setSubmitting }) => {
+    console.log('Submit:', values);
+    setSubmitting(false);
+  };
+  const validateFormValues = (values) => {
+    const { [fieldNames.min]: min, [fieldNames.max]: max } = values;
+    const errors = {};
+    if (Number(min) < minValue) {
+      errors.min = `Минимальное значение ниже допустимого: ${formatNumber(minValue)}`;
+    }
+    if (Number(max) > maxValue) {
+      errors.max = `Максимальное значение выше допустимого: ${formatNumber(maxValue)}`;
+    }
+    return errors;
+  };
+
   return (
     <div className={styles.attributeNumber}>
       <div className={styles.attributeNumberSection}>
-        <Input
-          value={minValue}
-        />
-        &nbsp;
-        -
-        &nbsp;
-        <Input
-          value={maxValue}
-        />
+        <Formik
+          initialValues={initialFormValues}
+          validate={validateFormValues}
+          onSubmit={handleSubmitForm}
+        >
+          {({ errors, submitForm }) => {
+            const errorMessages = Object.values(errors)
+              .map((message) => message);
+            const handleBlurInput = () => {
+              submitForm();
+            };
+            return (
+              <Form>
+                <div className={styles.attributeNumberRow}>
+                  <Field
+                    placeholder=""
+                    name={fieldNames.min}
+                    component={AttributeNumverInput}
+                    onBlur={handleBlurInput}
+                  />
+                  &nbsp;
+                  -
+                  &nbsp;
+                  <Field
+                    placeholder=""
+                    name={fieldNames.max}
+                    component={AttributeNumverInput}
+                    onBlur={handleBlurInput}
+                  />
+                </div>
+
+                <div className={styles.attributeNumberRow}>
+                  {errorMessages.map((message) => (
+                    <span
+                      key={message}
+                      className={styles.attributeNumberMessage}
+                    >
+                      {message}
+                    </span>
+                  ))}
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
       </div>
       <div className={styles.attributeNumberSection}>
         {children}
