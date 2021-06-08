e718ce2ba40f0b9c229a476b942adfd54dcf2c0b
Marat <mamirov@ubic.tech>
2021-06-08 13:51:59 +0300

    adv-29: SegmentsEdit/Attribute/AttributeNumber: update component

--- src/router/SegmentsEdit/Attribute/AttributeNumber/index.js
+++ src/router/SegmentsEdit/Attribute/AttributeNumber/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Input from '@/components/Input';
 import styles from './styles.module.scss';
 
 const propTypes = {
   children: PropTypes.node,
+  data: PropTypes.shape({
+    maxValue: PropTypes.string,
+    minValue: PropTypes.string,
+  }),
 };
 
 const defaultProps = {
   children: null,
+  data: {},
 };
 
 const AttributeNumber = function AttributeNumber({
   children,
+  data,
 }) {
+  const { maxValue, minValue } = data || {};
   return (
-    <div className={styles.wrapper}>
-      AttributeNumber
-      {children}
+    <div className={styles.attributeNumber}>
+      <div className={styles.attributeNumberSection}>
+        <Input
+          value={minValue}
+        />
+        &nbsp;
+        -
+        &nbsp;
+        <Input
+          value={maxValue}
+        />
+      </div>
+      <div className={styles.attributeNumberSection}>
+        {children}
+      </div>
     </div>
   );
 };
