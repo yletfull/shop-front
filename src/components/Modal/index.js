b6f0ca4eba6677adb6fb86168457d8682bec522a
Mikhail Panichev <denis.invader@gmail.com>
2021-07-08 12:23:14 +0300

    @/components/Modal: sizing, styles, onClose check

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -8,11 +8,15 @@ const propTypes = {
   className: PropTypes.string,
   title: PropTypes.node,
   children: PropTypes.node.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  width: PropTypes.number,
   onClose: PropTypes.func,
 };
 const defaultProps = {
   className: '',
   title: null,
+  size: 'medium',
+  width: 0,
   onClose: () => {},
 };
 
@@ -20,26 +24,50 @@ const Modal = function Modal({
   className,
   title,
   children,
+  size,
+  width,
   onClose,
 }) {
   const handleClose = onClose;
+  const isClosable = typeof handleClose === 'function';
+  const shouldRenderHeader = Boolean(title) && isClosable;
 
   return (
     <div className={cx(styles.wrapper, className)}>
-      <div className={styles.modal}>
-        <header className={styles.modalHeader}>
-          <span className={styles.modalHeaderTitle}>
-            {title}
-          </span>
+      {isClosable && (
+        <div
+          role="presentation"
+          className={styles.overlay}
+          onClick={handleClose}
+        />
+      )}
 
-          <button
-            type="button"
-            className={styles.modalCloseButton}
-            onClick={handleClose}
-          >
-            <IconTimesLight className={styles.modalCloseIcon} />
-          </button>
-        </header>
+      <div
+        className={cx(
+          styles.modal,
+          styles[`modal_size-${size}`],
+        )}
+        style={{
+          width: width ? `${width}rem` : null,
+        }}
+      >
+        {shouldRenderHeader && (
+          <header className={styles.modalHeader}>
+            <span className={styles.modalHeaderTitle}>
+              {title}
+            </span>
+
+            {isClosable && (
+              <button
+                type="button"
+                className={styles.modalCloseButton}
+                onClick={handleClose}
+              >
+                <IconTimesLight className={styles.modalCloseIcon} />
+              </button>
+            )}
+          </header>
+        )}
 
         <div className={styles.modalBody}>
           {children}
--- src/components/Modal/stories.js
+++ src/components/Modal/stories.js
@@ -12,6 +12,6 @@ const Template = (args) => (
 
 export const Playground = Template.bind({});
 Playground.args = {
-  title: 'Modal title adfasd afdsf asdf asdf asdf asdf asdf asdf asdf asdaf asdf asdf asdf asdf asdf asdf asdf asdaf sdaf asdf asdf asdf asdf asdf asd f',
+  title: 'Modal title',
   children: 'Modal content',
 };
