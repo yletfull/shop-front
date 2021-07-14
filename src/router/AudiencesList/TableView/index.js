92ed0658eb48f99ffa57ffc0f58b6a8a05413e71
Marat <mamirov@ubic.tech>
2021-07-14 12:30:09 +0300

    AudienceList/TableView: use formik

--- src/router/AudiencesList/TableView/index.js
+++ src/router/AudiencesList/TableView/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { useHistory } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import { Link } from 'react-router-dom';
+import { withFormikField } from '@/components/formik';
 import { useQuery } from '@/hooks';
 import { formatDate, formatNumber } from '@/utils/format';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
+import Select from '@/components/Select';
 import Spinner from '@/components/Spinner';
 import styles from './styles.module.scss';
 
@@ -32,158 +34,127 @@ const TableView = function TableView({
   isFetching,
   onFilter,
 }) {
+  const query = useQuery();
+
   const queryParams = {
-    searchDate: 'date',
     searchName: 'name',
     searchType: 'type',
   };
 
-  const history = useHistory();
-  const query = useQuery();
+  const initialFormValues = {
+    name: query.get(queryParams.searchName) || '',
+    type: query.get(queryParams.searchType) || '',
+  };
 
-  const [
-    searchDate,
-    setSearchDate,
-  ] = useState(query.get(queryParams.searchDate) || '');
-  const [
-    searchName,
-    setSearchName,
-  ] = useState(query.get(queryParams.searchName) || '');
-  const [
-    searchType,
-    setSearchType,
-  ] = useState(query.get(queryParams.searchType) || '');
+  const handleSubmitForm = (values) => {
+    console.log(values);
+    if (0) {
+      onFilter(values);
+    }
+  };
 
-  const handleChangeSearchDate = (e) => {
-    const { value } = e?.target || {};
-    if (typeof value === 'undefined') {
-      return;
-    }
-    setSearchDate(value);
-  };
-  const handleChangeSearchName = (e) => {
-    const { value } = e?.target || {};
-    if (typeof value === 'undefined') {
-      return;
-    }
-    setSearchName(value);
-  };
-  const handleChangeSearchType = (e) => {
-    const { value } = e?.target || {};
-    if (typeof value === 'undefined') {
-      return;
-    }
-    setSearchType(value);
-  };
-  const handleClickSearchButton = () => {
-    query.set(queryParams.searchDate, searchDate);
-    query.set(queryParams.searchName, searchName);
-    query.set(queryParams.searchType, searchType);
-    history.push({ search: query.toString() });
-    onFilter();
-  };
+  const FormikInput = withFormikField(Input);
+  const FormikSelect = withFormikField(Select);
 
   return (
-    <table className={styles.tableView}>
-      <tbody>
-        <tr>
-          <td data-purpose="filter">
-            <Input
-              className={cx(
-                styles.tableViewInput,
-                styles.tableViewInput_grow
-              )}
-              placeholder="Дата загрузки"
-              type="text"
-              value={searchDate}
-              onChange={handleChangeSearchDate}
-              fullwidth
-            />
-          </td>
-          <td data-purpose="filter">
-            <Input
-              className={cx(
-                styles.tableViewInput,
-                styles.tableViewInput_grow
-              )}
-              placeholder="Название"
-              type="text"
-              value={searchName}
-              onChange={handleChangeSearchName}
-              fullwidth
-            />
-          </td>
-          <td
-            colSpan="3"
-            data-purpose="filter"
-          >
-            <span className={styles.tableViewCell}>
-              <Input
-                className={cx(
-                  styles.tableViewInput,
-                  styles.tableViewInput_grow
-                )}
-                placeholder="Тип"
-                type="text"
-                value={searchType}
-                onChange={handleChangeSearchType}
-              />
-              <Button
-                type="button"
-                onClick={handleClickSearchButton}
-              >
-                Найти
-              </Button>
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            Название
-          </th>
-          <th>
-            Телефонов
-          </th>
-          <th>
-            E-mail
-          </th>
-          <th>
-            Тип
-          </th>
-          <th>
-            Дата загрузки
-          </th>
-        </tr>
+    <Formik
+      initialValues={initialFormValues}
+      onSubmit={handleSubmitForm}
+    >
+      {({ values }) => (
+        <Form>
+          <table className={styles.tableView}>
+            <tbody>
+              <tr>
+                <td
+                  colSpan="3"
+                  data-purpose="filter"
+                >
+                  <Field
+                    name="title"
+                    placeholder="Название"
+                    className={styles.tableViewInput}
+                    component={FormikInput}
+                    fullwidth
+                  />
+                </td>
+                <td
+                  colSpan="2"
+                  data-purpose="filter"
+                >
+                  <span className={styles.tableViewCell}>
+                    <Field
+                      name="type"
+                      placeholder="Тип"
+                      resetText="Сбросить"
+                      className={styles.tableViewInput}
+                      value={values.type}
+                      options={[
+                        { text: 'Глобальная', value: 0 },
+                        { text: 'Локальная', value: 1 },
+                      ]}
+                      component={FormikSelect}
+                    />
+                    <Button type="submit">
+                      Найти
+                    </Button>
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  Название
+                </th>
+                <th>
+                  Телефонов
+                </th>
+                <th>
+                  E-mail
+                </th>
+                <th>
+                  Тип
+                </th>
+                <th>
+                  Дата загрузки
+                </th>
+              </tr>
 
-        {isFetching && (
-          <tr>
-            <td colSpan="5">
-              <Spinner />
-            </td>
-          </tr>
-        )}
+              {isFetching && (
+                <tr>
+                  <td colSpan="5">
+                    <Spinner />
+                  </td>
+                </tr>
+              )}
 
-        {!isFetching && Array.isArray(data) && data.map((row) => (
-          <tr key={row.key}>
-            <td>
-              {row.title}
-            </td>
-            <td>
-              {row.emails ? formatNumber(row.emails) : '-'}
-            </td>
-            <td>
-              {row.phones ? formatNumber(row.phones) : '-'}
-            </td>
-            <td>
-              {row.local ? 'Локальная' : 'Глобальная'}
-            </td>
-            <td>
-              {formatDate(row.loadedAt)}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              {!isFetching && Array.isArray(data) && data.map((row) => (
+                <tr key={row.id || row.key}>
+                  <td>
+                    <Link
+                      to={`/audiences/details/${row.id || ''}`}
+                    >
+                      {row.title}
+                    </Link>
+                  </td>
+                  <td>
+                    {row.emails ? formatNumber(row.emails) : '-'}
+                  </td>
+                  <td>
+                    {row.phones ? formatNumber(row.phones) : '-'}
+                  </td>
+                  <td>
+                    {row.local ? 'Локальная' : 'Глобальная'}
+                  </td>
+                  <td>
+                    {formatDate(row.loadedAt)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
