e53eb6f27f998a9ba5a211b75014cdbeea235f69
Mikhail Panichev <denis.invader@gmail.com>
2021-07-15 17:45:15 +0300

    @/hooks: add use-service for requests

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useElementSize } from './use-element-size';
 export { default as useKeyPress } from './use-key-press';
 export { default as useOnClickOutside } from './use-on-click-outside';
 export { default as useQuery } from './use-query';
+export { default as useService } from './use-service';
