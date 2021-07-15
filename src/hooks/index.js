8543b205e981057bdafb8013de3d7e0afcd9a921
Mikhail Panichev <denis.invader@gmail.com>
2021-07-15 17:45:15 +0300

    @/hooks: add use-service for requests

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useDebounce } from './use-debounce';
 export { default as useKeyPress } from './use-key-press';
 export { default as useOnClickOutside } from './use-on-click-outside';
 export { default as useQuery } from './use-query';
+export { default as useService } from './use-service';
