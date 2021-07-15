7aa15e376a7b282c095bd7689585dd55056103ac
Mikhail Panichev <denis.invader@gmail.com>
2021-07-15 17:45:15 +0300

    @/hooks: add use-service for requests

---
+++ src/hooks/use-service/index.js
@@ -0,0 +1,58 @@
+import { useState, useRef, useCallback } from 'react';
+import axios from 'axios';
+
+const useService = function useServiceHook({
+  initialData = null,
+  autoCancel = true,
+  service,
+} = {}) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(initialData);
+  const cancelTokenSource = useRef(null);
+
+  const cancel = () => {
+    try {
+      cancelTokenSource.current.cancel();
+    } catch (thrown) {
+      // do nothing
+    }
+  };
+
+  const fetch = useCallback(async (params) => {
+    if (autoCancel) {
+      cancel();
+    }
+
+    cancelTokenSource.current = axios.CancelToken.source();
+
+    setIsFetching(true);
+    setError(null);
+
+    try {
+      const response = await service(
+        params,
+        { cancelToken: cancelTokenSource.current.token },
+      );
+
+      setData(response);
+    } catch (thrown) {
+      if (!axios.isCancel(thrown)) {
+        setError(thrown);
+      }
+    } finally {
+      setIsFetching(false);
+    }
+  }, [autoCancel, service]);
+
+  return {
+    fetch,
+    cancel,
+
+    isFetching,
+    error,
+    data,
+  };
+};
+
+export default useService;
