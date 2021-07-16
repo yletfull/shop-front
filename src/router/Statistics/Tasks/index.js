8e923d0eaedad54e4ddb8163af47682045204573
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-16 16:26:05 +0300

    @/router/Statistics/Tasks: add TableRows

--- src/router/Statistics/Tasks/index.js
+++ src/router/Statistics/Tasks/index.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { formatDate } from '@/utils/format';
 import { setHeader } from '@/store/ui/actions';
+import dayjs from '@/utils/day';
+import Table from '@/components/Table';
+import Header from '../components/Header';
+import TableRow from '../components/TableRow';
+import useFetch from '../hooks/use-fetch';
+
+const DATE_FORMAT = 'YYYY-MM-DD';
 
 const propTypes = {
   defaultTitle: PropTypes.string,
@@ -14,14 +22,40 @@ const defaultProps = {
 const StatisticsTasks = function StatisticsTaskScreen({ defaultTitle }) {
   const dispatch = useDispatch();
 
+  const { list, isFetching, error } = useFetch({
+    entity: 'tasks',
+    preventRequest: false,
+    dateStart: formatDate(dayjs().subtract(6, 'month'), DATE_FORMAT),
+    dateEnd: formatDate(dayjs(), DATE_FORMAT),
+  });
+
+  console.log({ isFetching, error });
+
   useEffect(() => {
     dispatch(setHeader(defaultTitle));
   }, [dispatch, defaultTitle]);
 
   return (
-    <div>
-      Задачи
-    </div>
+    <Table
+      header={<Header />}
+    >
+      {list.map((item) => (
+        <TableRow
+          key={item.id}
+          id={item.id}
+          index={item.index}
+          indexDiff={item.indexDiff}
+          name={item.name}
+          impressions={item.impressions}
+          clicks={item.clicks}
+          ctr={item.ctr}
+          positiveReactions={item.positiveReactions}
+          negativeReactions={item.negativeReactions}
+          repostsReactions={item.repostsReactions}
+          totalReactions={item.totalReactions}
+        />
+      ))}
+    </Table>
   );
 };
 
