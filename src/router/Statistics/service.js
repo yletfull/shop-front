2e3ffc7ff7630167c4a47f6bb198afebdd5fdb8b
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-17 18:31:30 +0300

    @/router/Statistics/service: fix fetching methods

--- src/router/Statistics/service.js
+++ src/router/Statistics/service.js
@@ -2,10 +2,12 @@ import api from '@/api';
 
 const API_URL = 'api/v1/statistics';
 
-const fetchList = ({ entity, params, cancelToken }) => api
+const generateFetchList = (entity) => (params, { cancelToken }) => api
   .get(`${API_URL}/${entity}`, { params, cancelToken })
   .then((data) => data);
 
+const fetchTasks = generateFetchList('tasks');
+
 export default {
-  fetchList,
+  fetchTasks,
 };
