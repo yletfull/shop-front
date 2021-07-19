55f3d6ab9b855c9ed27ca75cd97b2428d75c8679
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-19 18:02:18 +0300

    @/router/Statistics: add DateInputs

--- src/router/Statistics/index.js
+++ src/router/Statistics/index.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { formatDate } from '@/utils/format';
+import dayjs from '@/utils/day';
+import { useQuery } from '@/hooks';
 import { setHeader } from '@/store/ui/actions';
+import DateInputs from './components/DateInputs';
 import RouterView from './RouterView';
 import { paths, titles } from './routes';
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
+
+const DATE_FORMAT = 'YYYY-MM-DD';
 
 const propTypes = {
   defaultTitle: PropTypes.string,
@@ -18,12 +24,27 @@ const defaultProps = {
 const Statistics = function StatisticsScreen({ defaultTitle }) {
   const dispatch = useDispatch();
 
+  const query = useQuery();
+
+  const [params, setParams] = useState({
+    dateStart: query.get('dateStart') || formatDate(dayjs().subtract(6, 'month'), DATE_FORMAT),
+    dateEnd: query.get('dateEnd') || formatDate(dayjs(), DATE_FORMAT),
+  });
+
+  const handleDateInputsSubmit = (values) => setParams(values);
+
   useEffect(() => {
     dispatch(setHeader(defaultTitle));
   }, [dispatch, defaultTitle]);
 
   return (
     <div>
+      <DateInputs
+        className={styles.dateInputs}
+        dateStart={params.dateStart}
+        dateEnd={params.dateEnd}
+        onSubmit={handleDateInputsSubmit}
+      />
       <div className="nav-links-wrapper">
         {Object.keys(paths)
           .map((key) => (
@@ -37,7 +58,10 @@ const Statistics = function StatisticsScreen({ defaultTitle }) {
             </NavLink>
           ))}
       </div>
-      <RouterView />
+      <RouterView
+        dateStart={params.dateStart}
+        dateEnd={params.dateEnd}
+      />
     </div>
   );
 };
