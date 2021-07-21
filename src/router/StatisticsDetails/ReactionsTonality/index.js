ef57c9f73178a4db3c3ef674f78be0a734363b7b
Marat <mamirov@ubic.tech>
2021-07-21 14:34:01 +0300

    StatisticsDetails/ReactionsTonality: add chart axis ticks

--- src/router/StatisticsDetails/ReactionsTonality/index.js
+++ src/router/StatisticsDetails/ReactionsTonality/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { scaleLinear, scaleTime } from 'd3-scale';
 import { useElementSize } from '@/hooks';
-import { formatToDate } from '@/utils/format';
-import { XYArea } from '@/components/charts';
+import { formatDate, formatToDate } from '@/utils/format';
+import { XYArea, XYTicksX } from '@/components/charts';
 import {
   getReactionsTonalityData,
   getReactionsTonalityMeta,
@@ -12,9 +12,9 @@ import {
 import styles from './styles.module.scss';
 
 const padding = {
-  bottom: 0,
-  left: 0,
-  right: 0,
+  bottom: 32,
+  left: 32,
+  right: 32,
   top: 0,
 };
 
@@ -42,12 +42,39 @@ const ReactionsTonality = function ReactionsTonality({
   const scaleX = useMemo(() => scaleTime()
     .domain([formatToDate(dateStart), formatToDate(dateEnd)])
     .range([0, chartWidth]), [dateStart, dateEnd, chartWidth]);
+  const scaleY = useMemo(() => scaleLinear()
+    .domain([0, meta.maxPositive + meta.maxNegative])
+    .range([chartHeight, 0]), [chartHeight, meta]);
   const scaleYPositive = useMemo(() => scaleLinear()
     .domain([0, meta.maxPositive])
     .range([chartHeight / 2, 0]), [chartHeight, meta]);
   const scaleYNegative = useMemo(() => scaleLinear()
     .domain([0, meta.maxNegative])
     .range([0, chartHeight / 2]), [chartHeight, meta]);
+
+  /* eslint-disable react/function-component-definition */
+  const xTickRenderer = () => (value) => (
+    <text
+      key={value}
+      x={scaleX(value)}
+      y={chartHeight}
+      dy="1em"
+      textAnchor="center"
+    >
+      {formatDate(value)}
+    </text>
+  );
+  const xTickLineRenderer = () => (value) => (
+    <line
+      key={value}
+      x1={scaleX(value)}
+      y1="0"
+      x2={scaleX(value)}
+      y2={chartHeight}
+      stroke="hsla(0, 0%, 100%, .25)"
+    />
+  );
+  /* eslint-enable react/function-component-definition */
 
   return (
     <div className={styles.reactionsTonality}>
@@ -83,6 +110,22 @@ const ReactionsTonality = function ReactionsTonality({
               fill="#ff0000"
             />
           </g>
+          <XYTicksX
+            transform={`translate(${padding.left}, 0)`}
+            chartHeight={chartHeight}
+            scaleX={scaleX}
+            scaleY={scaleY}
+            ticksCount={5}
+            renderTick={xTickLineRenderer}
+          />
+          <XYTicksX
+            transform={`translate(${padding.left}, 0)`}
+            chartHeight={chartHeight}
+            scaleX={scaleX}
+            scaleY={scaleY}
+            ticksCount={2}
+            renderTick={xTickRenderer}
+          />
         </svg>
       </div>
     </div>
