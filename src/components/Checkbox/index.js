ce07e69e6110cb4b05228b13677a13ba1f0cc8d0
Mikhail Panichev <denis.invader@gmail.com>
2021-07-23 01:32:19 +0300

    @/components/Checkbox: upgrade

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,26 +1,107 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import IconCheck from '@/icons/Check';
 import styles from './styles.module.scss';
 
 const propTypes = {
   className: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.any),
+  id: PropTypes.string,
+  value: PropTypes.string,
+  disabled: PropTypes.bool,
+  checked: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+  children: PropTypes.node,
+  onChange: PropTypes.func,
 };
 
 const defaultProps = {
   className: '',
+  style: null,
+  id: null,
+  value: '',
+  disabled: false,
+  checked: false,
+  children: null,
+  onChange: () => {},
+};
+
+const computeIsChecked = (checked, value) => {
+  if (Array.isArray(checked)) {
+    return checked.includes(value);
+  }
+
+  if (typeof checked === 'string') {
+    return checked === value;
+  }
+
+  return checked;
 };
 
 const Checkbox = function Checkbox({
   className,
+  style,
+  id,
+  value,
+  disabled,
+  checked,
+  children,
+  onChange,
   ...props
 }) {
+  const isChecked = computeIsChecked(checked, value);
+  const handleChange = (e) => {
+    const nextChecked = e.target.checked;
+
+    if (Array.isArray(checked)) {
+      onChange(
+        nextChecked
+          ? [...checked, value]
+          : checked.filter((d) => d !== value),
+        e,
+      );
+      return;
+    }
+
+    if (typeof checked === 'string' && nextChecked) {
+      onChange(value, e);
+      return;
+    }
+
+    // typeof checked === 'boolean'
+    onChange(nextChecked, e);
+  };
+
   return (
-    <input
-      className={cx(styles.checkbox, className)}
-      type="checkbox"
-      {...props}
-    />
+    <label
+      htmlFor={id}
+      className={cx(className, styles.wrapper)}
+      style={style}
+    >
+      <input
+        {...props}
+        id={id}
+        className={styles.input}
+        type="checkbox"
+        value={value}
+        disabled={disabled}
+        checked={isChecked}
+        onChange={handleChange}
+      />
+      <span className={styles.box}>
+        <IconCheck className={styles.icon} />
+      </span>
+
+      {Boolean(children) && (
+        <span className={styles.label}>
+          {children}
+        </span>
+      )}
+    </label>
   );
 };
 
