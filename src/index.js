9ea37b16df26ac846b106e7e9185999add07fef4
Mikhail Panichev <denis.invader@gmail.com>
2021-07-25 21:27:19 +0300

    @/index: use app root node element from constants

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { rootNodes } from './constants/dom';
 import store from './store';
 import App from './App';
 import '@/theme/app-styles.scss';
@@ -12,5 +13,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </Router>,
-  document.getElementById('root'),
+  rootNodes.app,
 );
