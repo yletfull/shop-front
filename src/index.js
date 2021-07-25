b73d1c75b97c145916b8a6c2c1e567dad2a052bd
Mikhail Panichev <denis.invader@gmail.com>
2021-07-25 22:30:25 +0300

    @/index: import focus-visible polyfill

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'focus-visible';
 import { rootNodes } from './constants/dom';
 import store from './store';
 import App from './App';
