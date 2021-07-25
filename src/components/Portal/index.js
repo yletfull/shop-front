44b792dd364ad08ffab806dfb8bb1173b498d2fb
Mikhail Panichev <denis.invader@gmail.com>
2021-07-25 21:22:53 +0300

    @/components/Portal: target prop

--- src/components/Portal/index.js
+++ src/components/Portal/index.js
@@ -2,18 +2,27 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const propTypes = {
+  target: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.any,
+  ]).isRequired,
   children: PropTypes.node.isRequired,
 };
 
-const Portal = function Portal(props) {
-  const root = document.getElementById('portal-modals');
+const Portal = function Portal({
+  target,
+  children,
+}) {
+  const root = typeof target === 'string'
+    ? document.querySelector(target)
+    : target;
 
   if (!root) {
     return null;
   }
 
   return createPortal(
-    props.children,
+    children,
     root,
   );
 };
