17c6d053d3d9799535c6d9c42faf112c728e415e
Mikhail Panichev <denis.invader@gmail.com>
2021-07-25 22:09:27 +0300

    @/components/Modal: tall modal story for scroll testing

--- src/components/Modal/stories.js
+++ src/components/Modal/stories.js
@@ -24,10 +24,30 @@ const Template = (args) => {
     </div>
   );
 };
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper suscipit pellentesque. Maecenas pellentesque tincidunt suscipit. Vestibulum iaculis sapien eu nibh convallis egestas. Phasellus elementum arcu sed dolor pretium, id dapibus ex luctus. Pellentesque mauris leo, ullamcorper ac metus nec, tempor mattis justo. Praesent vel ipsum orci. Donec et cursus nisi. Proin molestie vitae metus ac fringilla. Vestibulum non orci quis leo dapibus facilisis. Quisque consectetur dui in est elementum, sed ornare enim lobortis. Sed vestibulum purus ligula, quis hendrerit mi auctor eget.';
 
 export const Playground = Template.bind({});
 Playground.args = {
   title: 'Modal title',
   children: 'Modal content',
+  isShown: true,
+};
+
+export const Tall = Template.bind({});
+Tall.args = {
+  title: 'Modal title',
+  children: (
+    <div>
+      <div>
+        {lorem}
+      </div>
+      <div style={{ marginTop: '4rem' }}>
+        {lorem}
+      </div>
+      <div style={{ marginTop: '4rem' }}>
+        {lorem}
+      </div>
+    </div>
+  ),
   isShown: true,
 };
