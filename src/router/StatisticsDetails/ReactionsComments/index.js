4832aaed0c26f61fa1ec2eede52c89f2c53cf165
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-26 18:06:04 +0300

    @/router/StatisticsDetails/ReactionsComments: add fetching in useService

--- src/router/StatisticsDetails/ReactionsComments/index.js
+++ src/router/StatisticsDetails/ReactionsComments/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import { useService } from '@/hooks';
+import WithSpinner from '../components/WithSpinner';
+import ErrorMessage from '../components/ErrorMessage';
+import service from '../service';
+import Chart from './Chart';
 import styles from './styles.module.scss';
 
 const propTypes = {
@@ -13,10 +19,44 @@ const ReactionsComments = function ReactionsComments({
   dateStart,
   dateEnd,
 }) {
-  console.log(dateStart, dateEnd);
+  const { entityType, id: entityId } = useParams();
+
+  const { fetch, data: response, isFetching, error } = useService({
+    initialData: {},
+    service: service.fetchReactionsComments,
+  });
+
+  useEffect(() => {
+    if (!dateStart || !dateEnd) {
+      return;
+    }
+    fetch(
+      {
+        entityType,
+        entityId,
+        params: { dateStart, dateEnd },
+      }
+    );
+  }, [fetch, dateStart, dateEnd, entityType, entityId]);
+
+  const data = Object.keys(response?.data || {})
+    .map((key) => response.data[key]);
+
+  console.log({ data });
+
   return (
     <div className={styles.wrapper}>
-      ReactionsComments
+      <WithSpinner
+        layout="overlay"
+        isFetching={isFetching}
+        className={styles.spinnerOverlay}
+      />
+      {error && (
+        <ErrorMessage
+          error={error}
+        />
+      )}
+      <Chart />
     </div>
   );
 };
