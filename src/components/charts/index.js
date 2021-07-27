6c4315dbfa42262611dd1aa77957708cfe161499
Marat <mamirov@ubic.tech>
2021-07-27 10:19:21 +0300

    components/charts: add XYBars component

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,4 +1,5 @@
 export { default as XYArea } from './XYArea';
+export { default as XYBars } from './XYBars';
 export { default as XYLine } from './XYLine';
 export { default as XYTicksX } from './XYTicksX';
 export { default as XYTicksY } from './XYTicksY';
