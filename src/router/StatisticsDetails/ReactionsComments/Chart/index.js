d783e8600d4738f75f019e048e3500264375d5f4
Marat <mamirov@ubic.tech>
2021-07-27 16:04:12 +0300

    StatisticsDetails/ReactionsComments: add axis y ticks

--- src/router/StatisticsDetails/ReactionsComments/Chart/index.js
+++ src/router/StatisticsDetails/ReactionsComments/Chart/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { scaleBand, scaleLinear } from 'd3-scale';
 import { useElementSize } from '@/hooks';
 import { getDatesRange } from '@/utils/day';
-import { formatToUnix } from '@/utils/format';
-import { XYBars } from '@/components/charts';
+import { formatNumber, formatToUnix } from '@/utils/format';
+import { XYBars, XYTicksY } from '@/components/charts';
 import { padding } from './constants';
 import styles from './styles.module.scss';
 
@@ -64,6 +64,30 @@ const ReactionsCommentsChart = function ReactionsCommentsChart({
     .domain([0, maxValue])
     .range([chartHeight, 0]), [chartHeight, maxValue]);
 
+  /* eslint-disable react/function-component-definition */
+  const yTickRenderer = () => (value) => (
+    <text
+      key={value}
+      className={styles.reactionsCommentsChartTickLabel}
+      x={0}
+      y={scaleY(value)}
+      dy=".35em"
+    >
+      {formatNumber(value)}
+    </text>
+  );
+  const yTickLineRenderer = () => (value) => (
+    <line
+      key={value}
+      className={styles.reactionsCommentsChartTickLine}
+      x1={0}
+      y1={scaleY(value)}
+      x2={chartWidth}
+      y2={scaleY(value)}
+    />
+  );
+  /* eslint-enable react/function-component-definition */
+
   return (
     <div
       ref={chartRef}
@@ -74,7 +98,17 @@ const ReactionsCommentsChart = function ReactionsCommentsChart({
         width={width}
         viewBox={`0 0 ${width} ${height}`}
       >
+        <XYTicksY
+          scaleY={scaleY}
+          ticksCount={4}
+          renderTick={yTickRenderer}
+        />
         <g transform={`translate(${padding.left}, ${padding.top})`}>
+          <XYTicksY
+            scaleY={scaleY}
+            ticksCount={4}
+            renderTick={yTickLineRenderer}
+          />
           {data.map((d) => (
             <g
               key={d.date}
