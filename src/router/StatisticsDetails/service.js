6df6b3f04232119450a318e2254beba2d83ddabe
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-27 17:55:35 +0300

    @/router/StatisticsDetails/service: add service for fb fetching

--- src/router/StatisticsDetails/service.js
+++ src/router/StatisticsDetails/service.js
@@ -87,6 +87,24 @@ const fetchReactionsTotal = function serviceFetchReactionsTotal({
     .then((response) => response.data.data);
 };
 
+const fetchReactionsByPlatform = function serviceFetchReactionsByPlatform({
+  platformId,
+  entityType,
+  entityId,
+  params,
+}) {
+  if (!entityType || !entityId) {
+    return;
+  }
+
+  const entity = encodeURIComponent(entityType);
+  const id = encodeURIComponent(entityId);
+
+  return api
+    .get(`${baseUrl}/${entity}/${id}/reactions/byPlatform/${platformId}`, { params })
+    .then((response) => response.data.data);
+};
+
 export default {
   fetchEntities,
   fetchEntityDynamics,
@@ -94,4 +112,5 @@ export default {
   fetchReactionsComments,
   fetchReactionsTonality,
   fetchReactionsTotal,
+  fetchReactionsByPlatform,
 };
