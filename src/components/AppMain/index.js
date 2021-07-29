223ce9de33f9dc31d4d9c4de8bbd0282f6445528
Marat <mamirov@ubic.tech>
2021-07-29 17:53:31 +0300

    components/AppHeader: add back link

--- src/components/AppMain/index.js
+++ src/components/AppMain/index.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AppHeader from '@/components/AppHeader';
 import styles from './styles.module.scss';
 
 const propTypes = {
+  backTo: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.string,
+    PropTypes.shape({
+      pathname: PropTypes.string,
+      search: PropTypes.string,
+      hash: PropTypes.string,
+      state: PropTypes.objectOf(PropTypes.any),
+    }),
+  ]),
   children: PropTypes.node,
   header: PropTypes.node,
 };
 
 const defaultProps = {
+  backTo: '',
   children: null,
   header: null,
 };
 
 const AppMain = function AppMain({
+  backTo,
   children,
   header,
 }) {
   return (
     <div className={styles.appMain}>
-      <div className={styles.appMainHeader}>
-        {header}
-      </div>
+      {header && (
+        <AppHeader backTo={backTo}>
+          {header}
+        </AppHeader>
+      )}
       <div className={styles.appMainContent}>
         {children}
       </div>
