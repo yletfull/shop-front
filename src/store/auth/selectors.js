21ebc087a24d0d3a67b2aa8ab4164eddfc46c535
Marat <mamirov@ubic.tech>
2021-07-30 14:10:48 +0300

    fetch user abilities

--- src/store/auth/selectors.js
+++ src/store/auth/selectors.js
@@ -1,23 +1,20 @@
 import { createSelector } from '@reduxjs/toolkit';
 import NS from './namespace';
 
-export const getState = (state) => state[NS] || {};
-export const getIsChecked = createSelector(
-  [getState],
-  (state) => state.isChecked,
-);
-export const getIsFetching = createSelector(
-  [getState],
-  (state) => state.isFetching,
-);
-export const getUser = createSelector(
-  [getState],
-  (state) => state.user,
-);
-export const getError = createSelector(
-  [getState],
-  (state) => state.error,
-);
+export const getIsChecked = (state) => (
+  state[NS]?.isChecked || false);
+export const getIsFetching = (state) => (
+  state[NS]?.isFetching || false);
+export const getError = (state) => (
+  state[NS]?.error || null);
+export const getUser = (state) => (
+  state[NS]?.user || null);
+
+export const getIsFetchingAbilities = (state) => (
+  state[NS]?.isFetchingAbilities || false);
+export const getAbilities = (state) => (
+  state[NS]?.abilities || []);
+
 export const getIsAuthorized = createSelector(
   [getUser],
   (user) => user && Object.keys(user).length > 0,
