1667f5af679934c8a0e8e49df08df0a79c16f497
Mikhail Panichev <denis.invader@gmail.com>
2021-08-01 22:56:29 +0300

    @/router/SegmentsEdit/components/SaveForm: confirm modal

--- src/router/SegmentsEdit/components/SaveForm/index.js
+++ src/router/SegmentsEdit/components/SaveForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Formik, Form, Field } from 'formik';
@@ -6,14 +6,17 @@ import { withFormikField } from '@/components/formik';
 import Input from '@/components/Input';
 import InputTextarea from '@/components/InputTextarea';
 import Button from '@/components/Button';
+import ConfirmModal from './ConfirmModal';
 import styles from './styles.module.scss';
 
 const propTypes = {
   className: PropTypes.string,
+  isSaving: PropTypes.bool,
   onSubmit: PropTypes.func,
 };
 const defaultProps = {
   className: '',
+  isSaving: false,
   onSubmit: () => {},
 };
 
@@ -26,52 +29,79 @@ const initialFormValues = {
 
 const SaveForm = function SegmentsSaveForm({
   className,
+  isSaving,
   onSubmit,
 }) {
+  const [isConfirmShown, setIsConfirmShown] = useState(false);
+  const [valuesToSave, setValuesToSave] = useState({});
   const handleFormSubmit = (values) => {
-    onSubmit(values);
+    setValuesToSave(values);
+    setIsConfirmShown(true);
   };
+  const handleSaveConfirm = () => {
+    onSubmit(valuesToSave);
+  };
+  const handleSaveCancel = () => {
+    setIsConfirmShown(false);
+  };
+  const shouldShowConfirm = isSaving || isConfirmShown;
+
+  useEffect(() => {
+    if (!isSaving) {
+      setIsConfirmShown(false);
+      setValuesToSave({});
+    }
+  }, [isSaving]);
 
   return (
-    <Formik
-      initialValues={initialFormValues}
-      onSubmit={handleFormSubmit}
-    >
-      {({ values }) => {
-        const canSubmit = Boolean(values.title);
+    <Fragment>
+      <Formik
+        initialValues={initialFormValues}
+        onSubmit={handleFormSubmit}
+      >
+        {({ values }) => {
+          const canSubmit = Boolean(values.title);
 
-        return (
-          <Form className={cx(className, styles.wrapper)}>
-            <div className={styles.row}>
-              <Field
-                name="title"
-                placeholder="Название"
-                required
-                fullwidth
-                component={FormikInput}
-              />
-            </div>
-            <div className={styles.row}>
-              <Field
-                name="description"
-                placeholder="Описание (не обязательно)"
-                fullwidth
-                component={FormikTextarea}
-              />
-            </div>
-            <div className={styles.row}>
-              <Button
-                type="submit"
-                className={styles.submit}
-                disabled={!canSubmit}
-              >
-                Сохранить
-              </Button>
-            </div>
-          </Form>
-        );
-      }}
-    </Formik>
+          return (
+            <Form className={cx(className, styles.wrapper)}>
+              <div className={styles.row}>
+                <Field
+                  name="title"
+                  placeholder="Название"
+                  required
+                  fullwidth
+                  component={FormikInput}
+                />
+              </div>
+              <div className={styles.row}>
+                <Field
+                  name="description"
+                  placeholder="Описание (не обязательно)"
+                  fullwidth
+                  component={FormikTextarea}
+                />
+              </div>
+              <div className={styles.row}>
+                <Button
+                  type="submit"
+                  className={styles.submit}
+                  disabled={!canSubmit}
+                >
+                  Сохранить
+                </Button>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+      {shouldShowConfirm && (
+        <ConfirmModal
+          isSaving={isSaving}
+          onConfirm={handleSaveConfirm}
+          onCancel={handleSaveCancel}
+        />
+      )}
+    </Fragment>
   );
 };
 
