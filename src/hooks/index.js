260f8a714642128eb9f628287c60f014fb7aa4d6
Marat <mamirov@ubic.tech>
2021-08-04 13:34:34 +0300

    hooks: add useQueryParams hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useKeyPress } from './use-key-press';
 export { default as useOnClickOutside } from './use-on-click-outside';
 export { default as useQuery } from './use-query';
 export { default as useService } from './use-service';
+export { default as useQueryParams } from './use-query-params';
