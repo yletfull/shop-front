a3eaa45cd4033b7ce0099ba46498b9b370f344ee
Marat <mamirov@ubic.tech>
2021-08-06 14:05:10 +0300

    components/Controls: update component

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import styles from './styles.module.scss';
 
 const propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 const defaultProps = {
   children: null,
+  className: '',
 };
 
-const Controls = function Controls({ children }) {
+const Controls = function Controls({
+  children,
+  className,
+  ...props
+}) {
   return (
-    <div className={styles.controls}>
+    <div
+      className={cx(styles.controls, className)}
+      {...props}
+    >
       {children}
     </div>
   );
