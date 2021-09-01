27ab47657410ec39a0fddb9e282be89656ae6953
Mikhail Panichev <denis.invader@gmail.com>
2021-09-02 00:15:03 +0300

    @/features/Statistics/pages/List: dates, nav tabs and header

--- src/features/Statistics/pages/List/index.js
+++ src/features/Statistics/pages/List/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import cx from 'classnames';
 import { useService, useQueryParams } from '@/hooks';
-import WidthSpinner from '@/components/WithSpinner';
+import AppMain from '@/components/AppMain';
+import Spinner from '@/components/Spinner';
 import ErrorMessageBlock from '@/components/ErrorMessageBlock';
+import DateInputs from '@/features/Statistics/components/DateInputs';
 import List from '@/features/Statistics/components/List';
 import { entities } from '@/features/Statistics/constants';
 import service from '@/features/Statistics/service';
@@ -14,21 +18,57 @@ const propTypes = {
       entity: PropTypes.oneOf(Object.values(entities)).isRequired,
     }).isRequired,
   }).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string,
+  }).isRequired,
 };
 
 const countOptions = [10, 20, 30];
+const navTabsLinks = Object
+  .values(entities)
+  .map((entity) => ({
+    entity,
+    title: ({
+      [entities.tasks]: 'Задачи',
+      [entities.campaigns]: 'ИК',
+      [entities.platforms]: 'Площадки',
+      [entities.sites]: 'Сайты',
+      [entities.spheres]: 'Сферы',
+    })[entity] || entity,
+  }));
 
 const StatisticsList = function StatisticsListScreen({
   match,
+  location,
 }) {
   const [queryParams, setQueryParams] = useQueryParams();
   const [filter, setFilter] = useState({ search: queryParams.search || '' });
+
+  const periodsService = useService({
+    initialData: [],
+    service: service.fetchPeriods,
+  });
+  const fetchPeriods = periodsService.fetch;
+  useEffect(() => {
+    fetchPeriods();
+  }, [fetchPeriods]);
+  const {
+    datestart: minDate,
+    dateend: maxDate,
+  } = periodsService.data[0] || {};
 
   const { fetch, data: response, isFetching, error } = useService({
     initialData: {},
     service: service.fetchList,
   });
 
+  const handleDateInputsSubmit = ({ dateStart, dateEnd }) => {
+    setQueryParams({
+      dateStart,
+      dateEnd,
+      currentPage: 1,
+    });
+  };
   const handlePageSelect = (currentPage) => setQueryParams({ currentPage });
   const handleCountSelect = (perPage) => {
     setQueryParams({
@@ -86,39 +126,92 @@ const StatisticsList = function StatisticsListScreen({
     currentPage,
     perPage,
   ]);
+  useEffect(() => {
+    if (!maxDate) {
+      return;
+    }
+
+    setQueryParams({
+      dateStart: maxDate,
+      dateEnd: maxDate,
+      currentPage: 1,
+    });
+  }, [maxDate, setQueryParams]);
 
   const { data, meta } = response || {};
 
-  const list = data || null;
-
   return (
-    <div className={styles.page}>
-      <WidthSpinner
-        className={styles.spinnerOverlay}
-        layout="overlay"
-        isFetching={isFetching}
-      />
-      {error && (
-        <ErrorMessageBlock
-          key="error-message"
-          error={error}
-        />
+    <AppMain
+      header={(
+        <div className={styles.header}>
+          <div className={styles.header_title}>
+            Статистика
+          </div>
+          <DateInputs
+            className={styles.dateInputs}
+            min={minDate}
+            max={maxDate}
+            values={{ dateStart, dateEnd }}
+            onChange={handleDateInputsSubmit}
+            onShift={handleDateInputsSubmit}
+            onSelect={handleDateInputsSubmit}
+          />
+          {periodsService.isFetching && <Spinner layout="inline" />}
+        </div>
       )}
-      <List
-        dateStart={dateStart}
-        dateEnd={dateEnd}
-        entity="tasks"
-        list={list}
-        pagination={meta?.pagination}
-        sort={meta?.sort}
-        filter={filter}
-        onFilterChange={handleFilterChange}
-        onPageSelect={handlePageSelect}
-        onCountSelect={handleCountSelect}
-        onSortChange={handleSortChange}
-        onFormSubmit={handleFormSubmit}
-      />
-    </div>
+    >
+      <div
+        className={cx([
+          styles.navigation,
+          'nav-links-wrapper',
+        ])}
+      >
+        {navTabsLinks.map((link) => (
+          <NavLink
+            key={link.entity}
+            to={{
+              pathname: `/statistics/lists/${link.entity}`,
+              search: location?.search || '',
+            }}
+            className="link-class_nav"
+            activeClassName="active-link-class_nav"
+          >
+            {link.title}
+          </NavLink>
+        ))}
+      </div>
+
+      <div className={styles.page}>
+        {isFetching && (
+          <Spinner
+            className={styles.spinnerOverlay}
+            layout="overlay"
+          />
+        )}
+
+        {error && (
+          <ErrorMessageBlock
+            key="error-message"
+            error={error}
+          />
+        )}
+
+        <List
+          dateStart={dateStart}
+          dateEnd={dateEnd}
+          entity="tasks"
+          list={data}
+          pagination={meta?.pagination}
+          sort={meta?.sort}
+          filter={filter}
+          onFilterChange={handleFilterChange}
+          onPageSelect={handlePageSelect}
+          onCountSelect={handleCountSelect}
+          onSortChange={handleSortChange}
+          onFormSubmit={handleFormSubmit}
+        />
+      </div>
+    </AppMain>
   );
 };
 
