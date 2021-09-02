9b8b12f2b6abb015930d38ac59aecfbab635ecb2
Mikhail Panichev <denis.invader@gmail.com>
2021-09-02 17:30:32 +0300

    @/components/SortButton: add dynamic story

--- src/components/SortButton/stories.js
+++ src/components/SortButton/stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SortButton from './index';
 
 export default {
@@ -14,4 +14,54 @@ Playground.args = {
   field: 'index',
   sortField: 'index',
   sortDir: 'asc',
+};
+
+export const Dynamic = () => {
+  const [sortDir, setSortDir] = useState('asc');
+  const [sortField, setSortField] = useState('name');
+  const handleSortChange = (fields) => {
+    setSortDir(fields.sortDir);
+    setSortField(fields.sortField);
+  };
+
+  return (
+    <div>
+      <SortButton
+        field="name"
+        sortField={sortField}
+        sortDir={sortDir}
+        onChange={handleSortChange}
+      >
+        Название
+      </SortButton>
+      {' '}
+      <SortButton
+        field="impressions"
+        sortField={sortField}
+        sortDir={sortDir}
+        onChange={handleSortChange}
+      >
+        Показы
+      </SortButton>
+      {' '}
+      <SortButton
+        field="clicks"
+        defaultSortDir="desc"
+        sortField={sortField}
+        sortDir={sortDir}
+        onChange={handleSortChange}
+      >
+        Клики
+      </SortButton>
+      {' '}
+      <SortButton
+        field="ctr"
+        sortField={sortField}
+        sortDir={sortDir}
+        onChange={handleSortChange}
+      >
+        CTR
+      </SortButton>
+    </div>
+  );
 };
