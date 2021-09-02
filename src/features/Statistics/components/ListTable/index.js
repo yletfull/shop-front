b13a61dc4cec675099676ac5609b63bfa01cd96f
Mikhail Panichev <denis.invader@gmail.com>
2021-09-02 13:40:08 +0300

    @/features/Statistics/components/ListTable: init component (WIP)

---
+++ src/features/Statistics/components/ListTable/index.js
@@ -0,0 +1,108 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Table from '@/components/Table';
+import RowData from './RowData';
+import RowFilter from './RowFilter';
+import RowHeader from './RowHeader';
+
+const metricShape = { diff: PropTypes.number, count: PropTypes.number };
+const propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      index: PropTypes.number.isRequired,
+      indexDiff: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      impressions: PropTypes.shape(metricShape).isRequired,
+      clicks: PropTypes.shape(metricShape).isRequired,
+      ctr: PropTypes.shape(metricShape).isRequired,
+      positiveReactions: PropTypes.shape(metricShape).isRequired,
+      negativeReactions: PropTypes.shape(metricShape).isRequired,
+      repostsReactions: PropTypes.shape(metricShape).isRequired,
+      totalReactions: PropTypes.shape(metricShape).isRequired,
+    }),
+  ).isRequired,
+  sort: PropTypes.shape({
+    sortDir: PropTypes.oneOf(['asc', 'desc']),
+    sortField: PropTypes.oneOf([
+      'impressions',
+      'clicks',
+      'ctr',
+      'positiveReactions',
+      'negativeReactions',
+      'repostsReactions',
+      'totalReactions',
+    ]),
+  }),
+  filters: PropTypes.shape({
+    search: PropTypes.string,
+  }),
+  onFiltersApply: PropTypes.func.isRequired,
+  onSortChange: PropTypes.func.isRequired,
+};
+const defaultProps = {
+  sort: {
+    sortDir: 'desc',
+    sortField: 'impressions',
+  },
+  filters: { search: '' },
+};
+
+const ListTable = function StatisticsListTable({
+  data,
+  sort,
+  filters,
+  onFiltersApply,
+  onSortChange,
+}) {
+  const [search, setSearch] = useState(filters?.search || '');
+  useEffect(() => {
+    setSearch(filters?.search || '');
+  }, [filters]);
+
+  const handleFiltersChange = (fields) => {
+    if ('search' in fields) {
+      setSearch(fields[search]);
+    }
+  };
+  const handleFiltersApply = (e) => {
+    e.preventDefault();
+    onFiltersApply({ search });
+  };
+  const handleSortChange = onSortChange;
+
+  return (
+    <form
+      action="."
+      method="get"
+      onSubmit={handleFiltersApply}
+    >
+      <Table
+        header={(
+          <Fragment>
+            <RowFilter
+              search={search}
+              onChange={handleFiltersChange}
+            />
+            <RowHeader
+              sort={sort}
+              onSortChange={handleSortChange}
+            />
+          </Fragment>
+        )}
+      >
+        {Array.isArray(data) && data.map((row) => (
+          <RowData
+            {...row}
+            key={row.id}
+          />
+        ))}
+      </Table>
+    </form>
+  );
+};
+
+ListTable.propTypes = propTypes;
+ListTable.defaultProps = defaultProps;
+
+export default ListTable;
