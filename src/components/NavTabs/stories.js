e36260327d1174b0649125f5cb1676bc124903fd
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-09-03 18:21:44 +0300

    @/components/NavTabs refactoring

--- src/components/NavTabs/stories.js
+++ src/components/NavTabs/stories.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
-import NavTabs from './index';
+import NavTabs, { NavTabLink } from './index';
 
 export default {
   title: 'Components/NavTabs',
   component: NavTabs,
 };
 
+const linksDataExample = [
+  {
+    key: 0,
+    path: '/test1/',
+    title: 'test1',
+    exact: true,
+  },
+  {
+    key: 1,
+    path: '/test2/',
+    title: 'test2',
+    exact: true,
+  },
+];
+
 const Template = (args) => (
   <MemoryRouter>
-    <NavTabs {...args} />
+    <NavTabs {...args}>
+      {linksDataExample?.map((props) => (
+        <NavTabLink
+          key={props.key}
+          {...props}
+        />
+      ))}
+    </NavTabs>
   </MemoryRouter>
 );
 
 export const Playground = Template.bind({});
 Playground.args = {
-  options: [
-    {
-      path: '/tasks/',
-      title: 'Задачи',
-    },
-    {
-      path: '/campaigns/',
-      title: 'Информационные компании',
-    },
-  ],
+  children: null,
   className: '',
 };
