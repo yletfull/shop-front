849f25749dbd751515b445720d72252d975d44da
Denis Shibidzhiev <dshibidzhiev@ubic.tech>
2021-09-24 12:17:59 +0300

    @/src/features/Statistics/pages/Details/components/EntityDynamics/utils.js: add getMultipliers

--- src/features/Statistics/pages/Details/components/EntityDynamics/utils.js
+++ src/features/Statistics/pages/Details/components/EntityDynamics/utils.js
@@ -10,7 +10,41 @@ const createArraysObject = (object) => {
   return result;
 };
 
-const formattedLines = createArraysObject(lines);
+const createNumbersObject = (object) => {
+  const result = {};
+
+  Object.values(object).forEach((value) => {
+    result[value] = 1;
+  });
+
+  return result;
+};
+
+const checkLengthAfterPoints = (number) => (10 ** Math.ceil(Math.log10(number))).toString().match(/\.(\d+)/)?.[1].length;
+
+const getMultiplier = (max) => {
+  let multiplier = 1;
+  let tmp = max;
+  let length = checkLengthAfterPoints(tmp);
+
+  if (tmp > 0 && tmp < 1 && tmp !== 0) {
+    console.log('length', length);
+    while (length > 1) {
+      multiplier /= 10;
+      length -= 1;
+    }
+  } else if (tmp > 1) {
+    while (tmp > 10) {
+      multiplier *= 10;
+      tmp /= 10;
+    }
+  }
+
+  return multiplier;
+};
+
+const formattedArrayLines = createArraysObject(lines);
+const formattedNumberLines = createNumbersObject(lines);
 
 export const getFormattedObject = (data) => (
   Object
@@ -24,7 +58,26 @@ export const getFormattedObject = (data) => (
           }
         });
       return acc;
-    }, formattedLines)
+    }, formattedArrayLines)
 );
 
-export const kek = () => true;
+
+export const getMultipliers = (data) => {
+  if (typeof data === 'undefined') { return 1; }
+
+  const maxValues = Object
+    .keys(data)
+    .reduce((acc, cur) => {
+      if (data[cur].length === 0) {
+        acc[cur] = 0;
+      } else {
+        acc[cur] = Math.max(...data[cur]);
+      }
+      return acc;
+    }, formattedNumberLines);
+
+
+  const multiplierCtr = getMultiplier(maxValues.ctr);
+
+  return multiplierCtr;
+};
