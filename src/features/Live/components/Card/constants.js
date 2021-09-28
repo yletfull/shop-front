c3f6f77b1ce2edc5fc9ed06bf6db6af4db7d7690
Andrey Korlyakov <akorlyakov@ubic.tech>
2021-09-28 14:58:50 +0300

    @/features/Live/components/Card/components/FbPromotedPost feature

--- src/features/Live/components/Card/constants.js
+++ src/features/Live/components/Card/constants.js
@@ -1,10 +1,14 @@
 import VkPromotedPost from './components/VkPromotedPost';
 import VkStory from './components/VkStory';
+import FbPromotedPost from './components/FbPromotedPost';
 
 export const platformsAdsTypes = {
   vk: {
     promotedPost: VkPromotedPost,
     story: VkStory,
+  },
+  fb: {
+    promotedPost: FbPromotedPost,
   },
 };
 
